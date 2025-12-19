5dc3a962678fb0f1ca354a154393f9e090edb395
JessieBaptista <jessielacerdabap@icloud.com>
2025-12-19 14:44:10 +0000

    cartas atualizadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,75 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char estado1;
+char codigo1[4];
+int populacao1;
+float area1;
+float pib1;
+int pontos1;
+
+char estado2;
+char codigo2[4];
+int populacao2;
+float area2;
+float pib2;
+int pontos2;
 
   // Área para entrada de dados
+printf("Cadastro da carta 1\n");
+printf("Estado (A a H): ");
+scanf(" %c", &estado1);
 
-  // Área para exibição dos dados da cidade
+printf("Codigo da carta (ex:A01): ");
+scanf("%s", codigo1);
+
+printf("Populacao: ");
+scanf("%d", &populacao1);
+
+printf("Area (km2): ");
+scanf("%f", &area1);
+
+printf("PIB: ");
+scanf("%f", &pib1);
+
+printf("Numero de pontos turisticos: ");
+scanf("%d", &pontos1);
+
+printf("\nCadastro da Carta 2\n");
+printf("Estado (A a H): ");
+scanf(" %c", &estado2);
+
+printf("Codigo da Carta (ex: B02): ");
+scanf("%s", codigo2);
+
+printf("Populacao: ");
+scanf("%d", &populacao2);
+
+printf("Area (km2): ");
+scanf("%f", &area2);
+
+printf("PIB: ");
+scan("%f", &pib2);
+
+printf("Numero de pontos turisticos: ");
+scanf("%d", &pontos2);
+
+// Área para exibição dos dados da cidade
+printf("\nCarta 1\n");
+printf("Estado: %c\n", estado1);
+printf("Codigo: %s\n", codigo1);
+printf("Populacao: %d\n", populacao1);
+printf("Area: %f Km2\n", area1);
+printf("PIB: %f\n", pib1);
+printf("Pontos Turisticos: %d\n", pontos1);
+
+printf("\nCarta 2\n");
+printf("Estado: %c\n", estado2);
+printf("Codigo: %s\n", codigo2);
+printf("Populacao: %d\n", populacao2);
+printf("Area? %f Km2\n", area2);
+printf("PIB: %f\n", pib2);
+printf("Pontos Turisticos: %d\n", pontos2);
 
 return 0;
 } 
