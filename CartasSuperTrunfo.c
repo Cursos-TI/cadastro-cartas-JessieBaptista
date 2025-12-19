a65e4d7aff176cb7f2daa1d6e3030ebc946c1f52
JessieBaptista <jessielacerdabap@icloud.com>
2025-12-19 17:56:42 +0000

    atualizado3

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,15 +6,19 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+// == CARTA 1 ==
 char estado1;
 char codigo1[4];
+char cidade[50];
 int populacao1;
 float area1;
 float pib1;
 int pontos1;
 
+// == CARTA 2 ==
 char estado2;
 char codigo2[4];
+char cidade2[50];
 int populacao2;
 float area2;
 float pib2;
@@ -22,11 +26,15 @@ int pontos2;
 
   // Área para entrada de dados
 printf("Cadastro da carta 1\n");
+
 printf("Estado (A a H): ");
 scanf(" %c", &estado1);
 
 printf("Codigo da carta (ex:A01): ");
 scanf("%s", codigo1);
+
+printf("Nome da Cidade: ");
+scanf(" %[^\n]", cidade1);
 
 printf("Populacao: ");
 scanf("%d", &populacao1);
@@ -41,11 +49,15 @@ printf("Numero de pontos turisticos: ");
 scanf("%d", &pontos1);
 
 printf("\nCadastro da Carta 2\n");
+
 printf("Estado (A a H): ");
 scanf(" %c", &estado2);
 
 printf("Codigo da Carta (ex: B02): ");
 scanf("%s", codigo2);
+
+printf("Nome da Cidade: ");
+scanf(" %[^\n]", cidade2);
 
 printf("Populacao: ");
 scanf("%d", &populacao2);
@@ -60,17 +72,21 @@ printf("Numero de pontos turisticos: ");
 scanf("%d", &pontos2);
 
 // Área para exibição dos dados da cidade
+printf("\n===== CARTAS CADASTRADAS =====\n");
+
 printf("\nCarta 1\n");
 printf("Estado: %c\n", estado1);
 printf("Codigo: %s\n", codigo1);
+printf("Cidade: %s\n", cidade1);
 printf("Populacao: %d\n", populacao1);
-printf("Area: %f Km2\n", area1);
-printf("PIB: %f\n", pib1);
+printf("Area: %.2f Km2\n", area1);
+printf("PIB: %2f\n", pib1);
 printf("Pontos Turisticos: %d\n", pontos1);
 
 printf("\nCarta 2\n");
 printf("Estado: %c\n", estado2);
 printf("Codigo: %s\n", codigo2);
+printf("Cidade: %s\n", cidade2);
 printf("Populacao: %d\n", populacao2);
 printf("Area: %.2f Km2\n", area2);
 printf("PIB: %.2f\n", pib2);
