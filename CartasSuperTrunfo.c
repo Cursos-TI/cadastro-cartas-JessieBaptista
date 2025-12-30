320d459b1902b9689c160f85e6c9ec9ee0ff16a1
JessieBaptista <jessielacerdabap@icloud.com>
2025-12-30 09:21:45 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,7 @@
-#include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 // == CARTA 1 ==
 char estado1;
 char codigo1[4];
-char cidade[50];
+char cidade1[50];
 int populacao1;
 float area1;
 float pib1;
@@ -23,6 +15,11 @@ int populacao2;
 float area2;
 float pib2;
 int pontos2;
+
+// Variaveis de calculo
+float densidade1, pibPerCapita1;
+float densidade2, pibPerCapita2;
+float superPoder1, superPoder2;
 
   // Área para entrada de dados
 printf("Cadastro da carta 1\n");
@@ -66,31 +63,44 @@ printf("Area (km2): ");
 scanf("%f", &area2);
 
 printf("PIB: ");
-scan("%f", &pib2);
+scanf("%f", &pib2);
 
 printf("Numero de pontos turisticos: ");
 scanf("%d", &pontos2);
+
+// --- Cálculos ---
+densidade1 = populacao1 / area1;
+pibPerCapita1 = pib1 / populacao1;
+
+densidade2 = populacao2 / area2;
+pibPerCapita2 = pib2 / populacao2;
+
+superPoder1 = (float)populacao1 + area1 + pib1 + pontos1 + pibPerCapita1 + (1.0 / densidade1);
+superPoder2 = (float)populacao2 + area2 + pib2 + pontos2 + pibPerCapita2 + (1.0 / densidade2);
 
 // Área para exibição dos dados da cidade
 printf("\n===== CARTAS CADASTRADAS =====\n");
 
 printf("\nCarta 1\n");
-printf("Estado: %c\n", estado1);
-printf("Codigo: %s\n", codigo1);
-printf("Cidade: %s\n", cidade1);
-printf("Populacao: %d\n", populacao1);
-printf("Area: %.2f Km2\n", area1);
-printf("PIB: %2f\n", pib1);
-printf("Pontos Turisticos: %d\n", pontos1);
+printf("Densidade Populacional: %.2f hab/km2\n", densidade1);
+printf("PIB per Capita: %.2f\n", pibPerCapita1);
+printf("Super Poder: %.2f\n", superPoder1);
 
 printf("\nCarta 2\n");
-printf("Estado: %c\n", estado2);
-printf("Codigo: %s\n", codigo2);
-printf("Cidade: %s\n", cidade2);
-printf("Populacao: %d\n", populacao2);
-printf("Area: %.2f Km2\n", area2);
-printf("PIB: %.2f\n", pib2);
-printf("Pontos Turisticos: %d\n", pontos2);
+printf("Densidade Populacional: %.2f hab/km2\n", densidade2);
+printf("PIB per Capita: %.2f\n", pibPerCapita2);
+printf("Super Poder: %.2f\n", superPoder2);
+
+// == COMPARACOES ==
+printf("\n== COMPARACAO DAS CARTAS ==\n");
+
+printf("Populacao: %s\n", populacao1 > populacao2 ? "Carta 1 venceu" : "Carta 2 venceu");
+printf("Area: %s\n", area1 > area2 ? "Carta 1 venceu" : "Carta 2 venceu");
+printf("PIB: %s\n", pib1 > pib2 ? "Carta 1 venceu" : "Carta 2 venceu");
+printf("Pontos Turisticos: %s\n", pontos1 > pontos2 ? "Carta 1 venceu" : "Carta 2 venceu");
+printf("PIB per Capita: %s\n", pibPerCapita1 > pibPerCapita2 ? "Carta 1 venceu" : "Carta 2 venceu");
+printf("Densidade Populacional: %s\n", densidade1 < densidade2 ? "Carta 1 venceu" : "Carta 2 venceu");
+printf("Super Poder: %s\n", superPoder1 > superPoder2 ? "Carta 1 venceu" : "Carta 2 venceu");
 
 return 0;
 } 
